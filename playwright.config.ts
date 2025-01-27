d40aed11d321364fb98c49d6e346685efc76d7e1
mcello23 <marceloadsc@gmail.com>
2025-01-27 20:08:47 +0100

    updating path to Allure reporter in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
       'allure-playwright',
       {
         detail: true,
-        outputFolder: 'allure-results',
+        outputFolder: 'test-results',
         suiteTitle: false,
       },
     ],
